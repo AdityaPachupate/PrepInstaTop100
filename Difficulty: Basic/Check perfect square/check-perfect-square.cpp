55bd3bf1775fdee778b3a265a015224910163041
Aditya  Pachupate <136146173+AdityaPachupate55@users.noreply.github.com>
2024-07-21 17:53:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Check perfect square/check-perfect-square.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    int checkPerfectSquare(int N){
+        // code here 
+        int val = sqrt(N);
+        
+        if(val*val == N) return 1;
+        return 0;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.checkPerfectSquare(N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
