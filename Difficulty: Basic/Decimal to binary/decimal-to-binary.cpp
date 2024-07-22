8a8e355204341a5815bbb14625b55904c2ece46e
Aditya  Pachupate <136146173+AdityaPachupate55@users.noreply.github.com>
2024-07-22 12:19:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Decimal to binary/decimal-to-binary.cpp
+++ Difficulty: Basic/Decimal to binary/decimal-to-binary.cpp
@@ -7,19 +7,22 @@ using namespace std;
 // } Driver Code Ends
 //User function Template for C++
 
-void toBinary(int N)
+void toBinary(int n)
 {
     // your code herewhi
-    int x = log2(N); 
-    string s="";
-    
-    
-    for(int i=x; i>=0;i--){
-        
-        s += to_string((N>>i)&1);
+    vector<int> rem;
+    int j =0;
+    while(n!=0){
+        rem.push_back(n%2);
+        n=n/2;
+        j++;
     }
-    cout<<s;
-        
+    
+    reverse(rem.begin(), rem.end());  
+    
+    for(auto it:rem){
+        cout<<it;
+    }
 }
 
 //{ Driver Code Starts.
